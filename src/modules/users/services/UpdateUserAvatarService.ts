8b80a7dccd6d863ea52140631c1fc0148c39b457
Caio Lucas <lienscarlet@gmail.com>
2020-07-13 15:22:57 -0300

    chore: faxina

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -2,9 +2,9 @@ import path from 'path';
 import fs from 'fs';
 import { getRepository } from 'typeorm';
 
-import User from '../models/User';
-import uploadConfig from '../config/upload';
-import AppError from '../errors/AppError';
+import AppError from '@shared/errors/AppError';
+import uploadConfig from '@config/upload';
+import User from '../infra/typeorm/entities/User';
 
 interface Request {
   user_id: string;
