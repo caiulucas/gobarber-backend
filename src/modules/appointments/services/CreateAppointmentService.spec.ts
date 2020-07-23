fe7109958782e4b811bf80bf52b8fbf4c8520631
Caio Lucas <lienscarlet@gmail.com>
2020-07-23 01:02:49 -0300

    chore: tests of list providers in month and list providers in day

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -16,11 +16,12 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
-      provider_id: '123123',
+      provider_id: 'provider_id',
+      user_id: 'user_id',
     });
 
     expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toBe('123123');
+    expect(appointment.provider_id).toBe('provider_id');
   });
 
   it('should not be able to create a new appointment on the same time', async () => {
@@ -28,13 +29,15 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
-      provider_id: '123123',
+      provider_id: 'provider_id',
+      user_id: 'user_id',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
-        provider_id: '123123',
+        provider_id: 'provider_id',
+        user_id: 'user_id',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
